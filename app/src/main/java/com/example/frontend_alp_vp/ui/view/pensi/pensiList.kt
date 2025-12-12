846576308dc135db04274a7f9f71fa85dc7e0f63
Angelique1701 <angeliquekw50@gmail.com>
2025-12-12 10:49:33 +0700

    pensi list draftm2

--- app/src/main/java/com/example/frontend_alp_vp/ui/view/pensi/pensiList.kt
+++ app/src/main/java/com/example/frontend_alp_vp/ui/view/pensi/pensiList.kt
@@ -1,15 +1,53 @@
 package com.example.frontend_alp_vp.ui.view.pensi
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.frontend_alp_vp.viewModel.pensiViewModel
 
 @Composable
 fun pensiList (viewModel: pensiViewModel) {
-    LazyColumn(){
-        Row(){
-            
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFE736B5))
+                .padding(vertical = 14.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowLeft,
+                contentDescription = "",
+                tint = Color.White
+            )
+            LazyColumn() {
+
+            }
         }
     }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun pensiListPreview (){
+    pensiList(
+        viewModel = TODO()
+    )
 }
