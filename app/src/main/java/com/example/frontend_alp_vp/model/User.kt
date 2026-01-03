487a08016090eb980a09bde16fa23051ebabc788
CelloYuwa <cmazzayuwa@student.ciputra.ac.id>
2026-01-03 17:58:06 +0700

    fix error

--- app/src/main/java/com/example/frontend_alp_vp/model/User.kt
+++ app/src/main/java/com/example/frontend_alp_vp/model/User.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 // This matches the "user" object inside your JSON responses
 data class UserData(
-    @SerializedName("id")
+    @SerializedName("userId")
     val id: Int,
 
     @SerializedName("name")
@@ -16,9 +16,9 @@ data class UserData(
     @SerializedName("email")
     val email: String,
 
-    @SerializedName("phone_number") // Standardized to snake_case for backend
+    @SerializedName("phoneNumber") // Standardized to snake_case for backend
     val phoneNumber: String?,
 
-    @SerializedName("profile_picture")
+    @SerializedName("profilePicture")
     val profilePhoto: String?
 )
