0430ee735b98e70883c34c0ff9f05c712d882839
Angelique1701 <angeliquekw50@gmail.com>
2026-01-07 10:25:46 +0700

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.media3:media3-exoplayer:1.2.0")
     implementation("androidx.media3:media3-ui:1.2.0")
-
+    implementation("io.coil-kt:coil-svg:2.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Untuk load gambar URL
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
